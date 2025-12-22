51cccca0a7fc126fab9b782aa25aa80774d4b3d0
gdiagilev <gdiagilev@yandex.ru>
2025-12-22 22:17:01 +0300

    Данные о фильмах и пользователях вынесены в отдельные хранилища. Пользователи могут добавлять друг друга в друзья и ставить фильмам лайки.

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -16,6 +16,24 @@ public class UserService {
 
     private final UserStorage userStorage;
 
+    private void validate(User user) {
+        if (user == null) {
+            throw new ValidationException("Пользователь не может быть null");
+        }
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            throw new ValidationException("Некорректный email");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Некорректный login");
+        }
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+    }
+
     public User create(User user) {
         validate(user);
         return userStorage.add(user);
@@ -34,28 +52,6 @@ public class UserService {
         return userStorage.getById(id)
                 .orElseThrow(() ->
                         new NotFoundException("Пользователь с id=" + id + " не найден"));
-    }
-
-    private void validate(User user) {
-        if (user == null) {
-            throw new ValidationException("Пользователь не может быть null");
-        }
-
-        if (user.getEmail() == null || !user.getEmail().contains("@")) {
-            throw new ValidationException("Некорректный email");
-        }
-
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Некорректный login");
-        }
-
-        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем");
-        }
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
     }
 
     public void addFriend(int userId, int friendId) {
