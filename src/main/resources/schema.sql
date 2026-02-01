297a316bff1a3358f8aca9245d3be08eccad466f
gdiagilev <gdiagilev@yandex.ru>
2026-02-01 16:27:38 +0300

    Добавлена база данных. Написаны мапперы и методы, позволяющие сохранять пользователей, фильмы, жанры и рейтинги в базу данных и получать их из неё. Получилось приложение, которое умеет обрабатывать и хранить данные о пользователях и их любимых фильмах.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,8 +4,19 @@ DROP TABLE IF EXISTS film_genres;
 DROP TABLE IF EXISTS films;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS mpa;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE mpa (
+    id INT PRIMARY KEY,
+    name VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE genres (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     login VARCHAR(255) NOT NULL UNIQUE,
@@ -13,29 +24,17 @@ CREATE TABLE IF NOT EXISTS users (
     birthday DATE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS films (
+CREATE TABLE films (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(200),
     release_date DATE NOT NULL,
     duration INT NOT NULL,
-    mpa_id INT NOT NULL
+    mpa_id INT NOT NULL,
+    CONSTRAINT fk_films_mpa FOREIGN KEY (mpa_id) REFERENCES mpa(id)
 );
 
-CREATE TABLE IF NOT EXISTS genres (
-    id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
-);
-
-INSERT INTO genres (id, name) VALUES
-(1,'Комедия'),
-(2,'Драма'),
-(3,'Мультфильм'),
-(4,'Триллер'),
-(5,'Документальный'),
-(6,'Боевик');
-
-CREATE TABLE IF NOT EXISTS film_genres (
+CREATE TABLE film_genres (
     film_id INT NOT NULL,
     genre_id INT NOT NULL,
     PRIMARY KEY (film_id, genre_id),
@@ -43,7 +42,7 @@ CREATE TABLE IF NOT EXISTS film_genres (
     CONSTRAINT fk_film_genres_genre FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS film_likes (
+CREATE TABLE film_likes (
     film_id INT NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY (film_id, user_id),
